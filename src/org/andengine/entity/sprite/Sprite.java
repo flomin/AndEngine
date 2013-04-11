4ac19e8882cfb0d66259c2c8d276416e0e1f8489
Florian Minjat <florian.minjat@zephir.org>
2013-04-11 11:00:17 +0200

    Sprite.markTextureRegionChanged() method added This method can be used after modifications to a Sprite texture in order to update the Sprite with a call to onUpdateTextureCoordinates().

--- src/org/andengine/entity/sprite/Sprite.java
+++ src/org/andengine/entity/sprite/Sprite.java
@@ -149,7 +149,7 @@ public class Sprite extends RectangularShape {
 	}
 
 	public void setFlipped(final boolean pFlippedHorizontal, final boolean pFlippedVertical) {
-		if((this.mFlippedHorizontal != pFlippedHorizontal) || (this.mFlippedVertical != pFlippedVertical)) {
+		if(this.mFlippedHorizontal != pFlippedHorizontal || this.mFlippedVertical != pFlippedVertical) {
 			this.mFlippedHorizontal = pFlippedHorizontal;
 			this.mFlippedVertical = pFlippedVertical;
 
@@ -204,13 +204,17 @@ public class Sprite extends RectangularShape {
 		this.mSpriteVertexBufferObject.onUpdateColor(this);
 	}
 
-	protected void onUpdateTextureCoordinates() {
-		this.mSpriteVertexBufferObject.onUpdateTextureCoordinates(this);
-	}
-
 	// ===========================================================
 	// Methods
 	// ===========================================================
+
+	protected void onUpdateTextureCoordinates() {
+		this.mSpriteVertexBufferObject.onUpdateTextureCoordinates(this);
+	}
+	
+	public void markTextureRegionChanged() {
+		onUpdateTextureCoordinates();
+	}
 
 	// ===========================================================
 	// Inner and Anonymous Classes
